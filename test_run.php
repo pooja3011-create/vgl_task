842cc1588d01f74434b83478b140c2bedec606a2
pooja <pp30111991@gmail.com>
2021-08-08 17:31:01 +0200

    changed function

--- test_run.php
+++ test_run.php
@@ -6,9 +6,7 @@ shell_exec('php cli/product_update.php 42 Staubsauger 150,00');
 shell_exec('php cli/product_delete.php 42');
 
 $output = shell_exec('php cli/event_worker.php');
-echo $output;
 if ($output === "Product created: 42 Staubsauger 100,00\nProduct updated: 150,00\nProduct deleted: 42\n") {
-   
     echo 'It works!' . PHP_EOL;
 } else {
     echo 'Something went wrong!' . PHP_EOL;
