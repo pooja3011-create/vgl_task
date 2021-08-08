92bc2c53a91558594a97b6904f8bab82b5e404be
pooja <pp30111991@gmail.com>
2021-08-08 17:29:59 +0200

    changed function

--- src/Storage/products.php
+++ src/Storage/products.php
@@ -2,7 +2,6 @@
 
 class Product
 {
-
     // Connection
     private $conn;
 
@@ -23,17 +22,17 @@ class Product
     // CREATE
     public function createProduct()
     {
-        $sqlQuery = "INSERT IGNORE INTO products (name, price)  
+        $sqlQuery = "INSERT IGNORE INTO products (name, price)
                 VALUES ('$this->name', '$this->price')";
-        if(mysqli_query($this->conn, $sqlQuery)){
+        if (mysqli_query($this->conn, $sqlQuery)) {
             $last_id = mysqli_insert_id($this->conn);
             return $last_id;
-        } else{
+        } else {
             echo "ERROR: Could not able to execute $sqlQuery. " . mysqli_error($this->conn);
         }
     }
     // UPDATE
-    public function updateProduct($price,$id)
+    public function updateProduct($price, $id)
     {
         $updateQuery = "UPDATE
                         " . $this->db_table . "
@@ -41,21 +40,20 @@ class Product
                         price = '$price'
                     WHERE
                         id = $id";
-            if(mysqli_query($this->conn, $updateQuery)){
-                return true;
-            } else{
-                echo "ERROR: Could not able to execute $updateQuery. " . mysqli_error($this->conn);
-            }
+        if (mysqli_query($this->conn, $updateQuery)) {
+            return true;
+        } else {
+            echo "ERROR: Could not able to execute $updateQuery. " . mysqli_error($this->conn);
+        }
     }
 
     // DELETE
     public function deleteProduct($id)
     {
-       
         $deleteQuery = "DELETE FROM " . $this->db_table . " WHERE id = $id";
-        if(mysqli_query($this->conn, $deleteQuery)){
+        if (mysqli_query($this->conn, $deleteQuery)) {
             return true;
-        } else{
+        } else {
             echo "ERROR: Could not able to execute $deleteQuery. " . mysqli_error($this->conn);
         }
     }
